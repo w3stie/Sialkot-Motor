ea9944e4fe4e66a3f337b13dee3ee7e4974eb121
w3stie <w3stie@users.noreply.github.com>
2024-07-16 13:06:39 +0300

    End homepage design

--- main.js
+++ main.js
@@ -3,3 +3,16 @@ let search = document.querySelector('.search-box');
 document.querySelector('#search-icon').onclick = () => {
     search.classList.toggle('active');
 }
+
+document.addEventListener('DOMContentLoaded', function () {
+    const menuIcon = document.getElementById('menu-icon');
+    const navbar = document.getElementById('navbar');
+
+    menuIcon.addEventListener('click', function () {
+        if (navbar.style.maxHeight) {
+            navbar.style.maxHeight = null;
+        } else {
+            navbar.style.maxHeight = navbar.scrollHeight + 'px';
+        }
+    });
+});
